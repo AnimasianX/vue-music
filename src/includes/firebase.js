5f5e0e33c872a9c215b287fcfcbbc230015726ab
Jimmy Lin <jlin3205@gmail.com>
2023-07-13 00:19:57 -0400

    Added firebase app and auth to project - need to get config for tomorrow

---
+++ src/includes/firebase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {
+"Get it from firebase from google"
+}
+
+export default firebase.initializeApp(firebaseConfig)
