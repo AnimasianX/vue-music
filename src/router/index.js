d3b326b0379b2041b2b7e371bcf9bb1939a36f8d
AnimasianX <jlin2744@gmail.com>
2023-07-20 21:43:16 -0400

    Added routing guard rails to prevent unauthenticated user from accessing authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,51 @@ import {
 
 import Home from '@/views/HomeView.vue';
 import About from '@/views/AboutView.vue';
+import Manage from '@/views/ManageView.vue';
 
 const routes = [{
+  name: 'home',
   path: "/",
   component: Home,
 },
 {
+  name: 'about',
   path: "/about",
   component: About,
-}];
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: "/manage-music",
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    }
+  },
+  {
+    path: "/manage",
+    redirect: { name: "manage" }
+  },
+
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: 'home' }
+  }
+
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 })
+
+router.beforeEach((to, from, next) => {
+  console.log("Global Guard");
+
+  // console.log(to, from);
+  next();
+});
 
 export default router
